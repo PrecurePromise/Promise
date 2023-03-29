974b6a11d3dc22bc53314c8194622cb362c37c2a
서요셉 <tjdytpq0310@gmail.com>
2023-03-29 18:05:32 +0900

    :recycle: REFACTOR: 내 알람 리스트 조회 메소드 타입, 네이밍 변경

--- backend/src/main/java/com/pjt3/promise/service/MyPillService.java
+++ backend/src/main/java/com/pjt3/promise/service/MyPillService.java
@@ -1,15 +1,17 @@
 package com.pjt3.promise.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.pjt3.promise.entity.User;
 import com.pjt3.promise.response.MyAlarmHistory;
 import com.pjt3.promise.response.MyPillGetRes;
 import com.pjt3.promise.response.MyPillHistoryGetRes;
+import org.springframework.security.core.Authentication;
 
 public interface MyPillService {
 
-	List<MyPillGetRes> getMyPillList(User user);
+	Map<String, List> getMyPillMap(Authentication authentication);
 
 	MyPillHistoryGetRes getMyPillHistoryList(User user, int pageNum);
 
