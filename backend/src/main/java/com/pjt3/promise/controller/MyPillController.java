c900a7432269f9f6edd2d221d9a93b570649961e
서요셉 <tjdytpq0310@gmail.com>
2023-03-29 18:07:09 +0900

    :recycle: REFACTOR: Service 로직 변경에 따른 코드 수정

--- backend/src/main/java/com/pjt3/promise/controller/MyPillController.java
+++ backend/src/main/java/com/pjt3/promise/controller/MyPillController.java
@@ -1,25 +1,19 @@
 package com.pjt3.promise.controller;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pjt3.promise.common.auth.PMUserDetails;
 import com.pjt3.promise.common.response.BaseResponseBody;
 import com.pjt3.promise.entity.User;
-import com.pjt3.promise.repository.UserRepository;
-import com.pjt3.promise.response.MyPillGetRes;
 import com.pjt3.promise.response.MyPillHistoryGetRes;
 import com.pjt3.promise.service.MyPillService;
 
@@ -33,23 +27,9 @@ public class MyPillController {
 	@GetMapping()
 	public ResponseEntity<?> getMyPillList(Authentication authentication) {
 
-		try {
+		Map<String, List> alarmMap = myPillService.getMyPillMap(authentication);
 
-			PMUserDetails userDetails = (PMUserDetails) authentication.getDetails();
-			User user = userDetails.getUser();
-
-			List<MyPillGetRes> alarmList = myPillService.getMyPillList(user);
-			
-			Map<String, List> map = new HashMap<String, List>();
-			map.put("alarmList", alarmList);
-			
-			return ResponseEntity.status(200).body(map);
-
-		} catch (NullPointerException e) {
-			return ResponseEntity.status(420).body(BaseResponseBody.of(420, "만료된 토큰입니다."));
-		} catch (Exception e) {
-			return ResponseEntity.status(500).body(BaseResponseBody.of(500, "Internal Server Error"));
-		} 
+		return ResponseEntity.status(200).body(alarmMap);
 
 	}
 	
